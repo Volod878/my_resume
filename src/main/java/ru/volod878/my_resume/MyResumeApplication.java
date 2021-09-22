2985b862576cfbf6808a53d44025d7b6b6f4f240
Volod878 <volod1988@gmail.com>
2021-09-22 20:21:20 +0700

    Init

---
+++ src/main/java/ru/volod878/my_resume/MyResumeApplication.java
@@ -0,0 +1,13 @@
+package ru.volod878.my_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyResumeApplication.class, args);
+    }
+
+}
